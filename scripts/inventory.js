78aa86904bddb27fe1527abc78e76d71446d2653
trashfacu <fm.mazziotti@gmail.com>
2022-12-27 20:37:28 -0300

    Alpha minimal functions

---
+++ scripts/inventory.js
@@ -0,0 +1,85 @@
+// Arr of clicker's elements
+
+export const ObjetoClick = {
+  cup: 0,
+  cupPerSecond: 0,
+  cupsPerClick: 1,
+  totalCups: 0,
+};
+
+// INVENTARIO
+
+export const inventario = [
+  {
+    nombre: "Molina",
+    nombreDeClase: "molina",
+    costo: 15,
+    cantidad: 0,
+    aumento: 0.1,
+    imagen: "../assets/sprites_inv/test.png",
+  },
+  {
+    nombre: "Tagliafico",
+    nombreDeClase: "tagliafico",
+    costo: 100,
+    cantidad: 0,
+    aumento: 1,
+    imagen: "../assets/sprites_inv/test.png",
+  },
+  {
+    nombre: "De Paul",
+    nombreDeClase: "dePaul",
+    costo: 1100,
+    cantidad: 0,
+    aumento: 8,
+    imagen: "../assets/sprites_inv/test.png",
+  },
+  {
+    nombre: "Mac Allister",
+    nombreDeClase: "macAllister",
+    costo: 12000,
+    cantidad: 0,
+    aumento: 47,
+    imagen: "../assets/sprites_inv/test.png",
+  },
+  {
+    nombre: "Cuti Romero",
+    nombreDeClase: "cutiRomero",
+    costo: 130000,
+    cantidad: 0,
+    aumento: 260,
+    imagen: "../assets/sprites_inv/test.png",
+  },
+  {
+    nombre: "Otamendi",
+    nombreDeClase: "otamendi",
+    costo: 1400000,
+    cantidad: 0,
+    aumento: 1400,
+    imagen: "../assets/sprites_inv/test.png",
+  },
+  {
+    nombre: "Julian Alvarez",
+    nombreDeClase: "julianAlvarez",
+    costo: 20000000,
+    cantidad: 0,
+    aumento: 7800,
+    imagen: "../assets/sprites_inv/test.png",
+  },
+  {
+    nombre: "Di Maria",
+    nombreDeClase: "diMaria",
+    costo: 330000000,
+    cantidad: 0,
+    aumento: 44000,
+    imagen: "../assets/sprites_inv/test.png",
+  },
+  {
+    nombre: "Messi",
+    nombreDeClase: "messi",
+    costo: 5100000000,
+    cantidad: 0,
+    aumento: 260000,
+    imagen: "../assets/sprites_inv/test.png",
+  },
+];
---
+++ scripts/main.js
@@ -0,0 +1,115 @@
+import { inventario, ObjetoClick } from "./inventory.js";
+//Formating and styling
+
+function formatCupValue(number) {
+  return Math.round(number * 10) / 10;
+}
+
+function formatCostValue(value) {
+  if (value >= 1000000) {
+    let formattedValue = (value / 1000000).toFixed(1);
+    formattedValue = formattedValue.replace(".0", "");
+    formattedValue += "M";
+    return formattedValue;
+  }
+  return value;
+}
+
+//Creation of the buildings
+for (let data in inventario) {
+  let divContainer = document.getElementById("buildingsContainer");
+  const buildingHTML = `
+  <div id="${inventario[data].nombreDeClase}" class="buildingStyle">
+    <img src="${inventario[data].imagen}" alt="${
+    inventario[data].nombreDeClase
+  }" id="sprite" />
+    <p id="${inventario[data].nombreDeClase}">${inventario[data].nombre}</p>
+    <p id="show${inventario[data].nombre}Cost">${formatCostValue(
+    inventario[data].costo
+  )}</p>
+    <p id="show${inventario[data].nombre}Cant">0</p>
+  </div>
+`;
+  divContainer.innerHTML += buildingHTML;
+}
+
+//Increment counter and display it.
+
+function counterCupIncrease() {
+  ObjetoClick.cup += ObjetoClick.cupsPerClick; //add the quantity of counter per click to the total
+  document.getElementById("showCounter").innerText = ObjetoClick.cup;
+}
+
+window.setInterval(function () {
+  ObjetoClick.cup += ObjetoClick.cupPerSecond;
+  document.getElementById("showCounter").innerText = formatCupValue(
+    ObjetoClick.cup
+  );
+}, 1000);
+
+let btnIncreaseCounting = document.getElementById("clickToIncrease");
+btnIncreaseCounting.addEventListener("click", counterCupIncrease);
+
+function nextCost(baseCost, quantity) {
+  return (baseCost = Math.round(baseCost * Math.pow(1.15, quantity)));
+}
+
+function buyBuilding(index) {
+  if (ObjetoClick.cup >= inventario[index].costo) {
+    ObjetoClick.cup -= inventario[index].costo; // subtract the amount of cups corresponding to the cost of the building
+    document.getElementById("showCounter").innerText = formatCupValue(
+      ObjetoClick.cup
+    ); // shows them in the counter
+    inventario[index].cantidad++; //add +1 to the building quantity
+    document.getElementById(
+      "show" + inventario[index].nombre + "Cant"
+    ).innerText = inventario[index].cantidad; //show it in the building counter
+    ObjetoClick.cupPerSecond += inventario[index].aumento; //increase the number of cups generated per second
+    document.getElementById("showCounterPerSecond").innerText = formatCupValue(
+      ObjetoClick.cupPerSecond
+    ); //shows it in the counter
+    let nextCostBuilding = nextCost(
+      inventario[index].costo,
+      inventario[index].cantidad
+    ); // increase the next cost
+    document.getElementById(
+      "show" + inventario[index].nombre + "Cost"
+    ).innerText = nextCostBuilding; // updates it in the DOM
+  } else {
+    console.log("Te faltan copas");
+  }
+}
+
+let btnBuyBuilding = document.getElementsByClassName("buildingStyle");
+
+for (let i = 0; i < btnBuyBuilding.length; i++) {
+  btnBuyBuilding[i].addEventListener("click", function () {
+    buyBuilding(i);
+  });
+}
+
+// //Test Upgrades
+// const upgrades = [
+//   {
+//     name: "Upgrade 1",
+//     description: "This is the first upgrade",
+//     cost: 1000,
+//   },
+//   {
+//     name: "Upgrade 2",
+//     description: "This is the second upgrade",
+//     cost: 2000,
+//   },
+//   {
+//     name: "Upgrade 3",
+//     description: "This is the third upgrade",
+//     cost: 3000,
+//   },
+// ];
+// const upgradeListElement = document.getElementById("upgradeList");
+// for (let i = 0; i < upgrades.length; i++) {
+//   const li = document.createElement("li");
+//   li.innerHTML = `${upgrades[i].name}: ${upgrades[i].description} <button onclick="buyUpgrade(${i})">Buy</button>`;
+
+//   upgradeListElement.appendChild(li);
+// }
