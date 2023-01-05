fcfd2e3a5c05daae8f2ec87b98c2a76eae03f78b
trashfacu <fm.mazziotti@gmail.com>
2023-01-05 18:27:05 -0300

    Rerouting sprites

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -17,7 +17,7 @@ export let inventario = [
     costo: 15,
     cantidad: 0,
     aumento: 0.1,
-    imagen: "../assets/sprites_inv/23 Emiliano Martinez 04.jpg",
+    imagen: "/assets/sprites_inv/23 Emiliano Martinez 04.jpg",
   },
   {
     nombre: "Nahuel Molina",
@@ -26,7 +26,7 @@ export let inventario = [
     costoBase: 100,
     cantidad: 0,
     aumento: 1,
-    imagen: "../assets/sprites_inv/26 Molina 02.jpg",
+    imagen: "/assets/sprites_inv/26 Molina 02.jpg",
   },
   {
     nombre: "Nicolás Otamendi",
@@ -35,16 +35,16 @@ export let inventario = [
     costoBase: 1100,
     cantidad: 0,
     aumento: 8,
-    imagen: "../assets/sprites_inv/19 Otamendi 02.jpg",
+    imagen: "/assets/sprites_inv/19 Otamendi 02.jpg",
   },
   {
-    nombre: "Lisandro Martínez",
-    nombreDeClase: "lMartinez",
+    nombre: "Nicolas Tagliafico",
+    nombreDeClase: "nTagliafico",
     costo: 12000,
     costoBase: 12000,
     cantidad: 0,
     aumento: 47,
-    imagen: "../assets/sprites_inv/25 Lisandro Martinez 01.jpg",
+    imagen: "/assets/sprites_inv/3 Tagliafico 02.jpg",
   },
   {
     nombre: "Ángel Di María",
@@ -53,16 +53,16 @@ export let inventario = [
     costoBase: 130000,
     cantidad: 0,
     aumento: 260,
-    imagen: "../assets/sprites_inv/11 Di Maria 03.jpg",
+    imagen: "/assets/sprites_inv/11 Di Maria 03.jpg",
   },
   {
-    nombre: "Marcos Acuña",
-    nombreDeClase: "acuña",
+    nombre: "Alejandro Gomez",
+    nombreDeClase: "aGomez",
     costo: 1400000,
     costoBase: 1400000,
     cantidad: 0,
     aumento: 1400,
-    imagen: "../assets/sprites_inv/Acuña01.jpg",
+    imagen: "/assets/sprites_inv/17 Gomez 02.jpg",
   },
   {
     nombre: "Rodrigo De Paul",
@@ -71,7 +71,7 @@ export let inventario = [
     costoBase: 20000000,
     cantidad: 0,
     aumento: 7800,
-    imagen: "../assets/sprites_inv/7 De Paul 01.jpg",
+    imagen: "/assets/sprites_inv/7 De Paul 01.jpg",
   },
   {
     nombre: "Enzo Fernández",
@@ -80,7 +80,7 @@ export let inventario = [
     costoBase: 330000000,
     cantidad: 0,
     aumento: 44000,
-    imagen: "../assets/sprites_inv/24 Fernandez 01.jpg",
+    imagen: "/assets/sprites_inv/24 Fernandez 01.jpg",
   },
   {
     nombre: "Alexis Mac Allister",
@@ -89,7 +89,7 @@ export let inventario = [
     costoBase: 5100000000,
     cantidad: 0,
     aumento: 260000,
-    imagen: "../assets/sprites_inv/20 Mac Allister 02.jpg",
+    imagen: "/assets/sprites_inv/20 Mac Allister 02.jpg",
   },
   {
     nombre: "Julián Álvarez",
@@ -98,7 +98,7 @@ export let inventario = [
     costoBase: 75000000000,
     cantidad: 0,
     aumento: 1600000,
-    imagen: "../assets/sprites_inv/9 Alvarez 01.jpg",
+    imagen: "/assets/sprites_inv/9 Alvarez 01.jpg",
   },
   {
     nombre: "Lionel Messi",
@@ -107,7 +107,7 @@ export let inventario = [
     costoBase: 1000000000000,
     cantidad: 0,
     aumento: 10000000,
-    imagen: "../assets/sprites_inv/10 Messi 02.jpg",
+    imagen: "/assets/sprites_inv/10 Messi 02.jpg",
   },
   {
     nombre: "Lionel Scaloni",
@@ -116,7 +116,7 @@ export let inventario = [
     costoBase: 14000000000000,
     cantidad: 0,
     aumento: 65000000,
-    imagen: "../assets/sprites_inv/DT Scaloni 01.jpg",
+    imagen: "/assets/sprites_inv/DT Scaloni 01.jpg",
   },
 ];
 
